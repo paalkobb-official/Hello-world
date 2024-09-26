2f523b42e4015bebf5fda91c404ae6cd9d42cdbd
Paal Kobbeltvedt <paalkobbeltvedt@hotmail.com>
2024-09-26 09:49:09 +0200

    Hello world init

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HelloWorld 
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
